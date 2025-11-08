5bf89e931c5219233efaa06c53f0e9e7d4c652af
eduardo <eduardomendes418@gmail.com>
2025-11-08 08:48:42 -0300

    [CMS-12]fix - create internalization en, es and pt

---
+++ frontend/src/i18n/index.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './translations/en.json';
+import pt from './translations/pt.json';
+import es from './translations/es.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  pt: {
+    translation: pt,
+  },
+  es: {
+    translation: es,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'pt', 
+    fallbackLng: 'en', 
+    interpolation: {
+      escapeValue: false, 
+    },
+  });
+
+export default i18n;
