5da097500841bea5012f64797fb0da41a31b7cbb
eduardo <eduardomendes418@gmail.com>
2025-11-12 08:20:53 -0300

    [CMS-19]fix - format I18n

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,31 +1,29 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import en from './translations/en.json';
-import pt from './translations/pt.json';
-import es from './translations/es.json';
+import en from "./translations/en.json";
+import pt from "./translations/pt.json";
+import es from "./translations/es.json";
 
 const resources = {
-  en: {
-    translation: en,
-  },
-  pt: {
-    translation: pt,
-  },
-  es: {
-    translation: es,
-  },
+	en: {
+		translation: en,
+	},
+	pt: {
+		translation: pt,
+	},
+	es: {
+		translation: es,
+	},
 };
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: 'pt', 
-    fallbackLng: 'en', 
-    interpolation: {
-      escapeValue: false, 
-    },
-  });
+i18n.use(initReactI18next).init({
+	resources,
+	lng: "pt",
+	fallbackLng: "en",
+	interpolation: {
+		escapeValue: false,
+	},
+});
 
 export default i18n;
